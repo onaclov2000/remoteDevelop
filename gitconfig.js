8ae4e2e431de7c009247b342b8195b05944cc74a
Tyson Bailey <onaclov2000+github@gmail.com>
2015-01-30 13:03:59 -0700

    Create gitconfig.js

---
+++ gitconfig.js
@@ -0,0 +1,5 @@
+// probably a really poor name, but it works.
+module.exports = {
+   FB_URL : 'https://onaclovtech-home.firebaseio.com/github/webdvr/commits',
+   GIT_REPO : 'https://github.com/onaclov2000/webdvr.git'
+}
