cdb2a31ba8c8461fe5353faebe92c558d38c92c5
Tyson Bailey <onaclov2000+github@gmail.com>
2015-10-15 22:49:18 -0600

    Example versino that compiles java creates jar, and runs test.

--- remote.js
+++ remote.js
@@ -13,15 +13,61 @@ myRootRef.limitToLast(1).on('child_added', function(childSnapshot, prevChildName
    // code to handle child data changes.
    var data = childSnapshot.val();
    var localref = childSnapshot.ref();
-
+    console.log("got here")
     if (initialize){
-       if (data["commits"][0]["modified"][0] == "app.js") {
           console.log("Pull me some new code");
-          result    = spawn('git', ['pull', GIT_REPO]);
-          result.on('close', function (code) {
-            console.log("Got the data good");
+          pull    = spawn('git', ['pull', GIT_REPO]);
+          pull.on('close', function (code) {
+            console.log("Clone Complete");
+            // re-run ant, then re-run the test.
+            ant    = spawn('ant');
+            ant.stderr.on('data',function(buf) {
+	          console.log(buf.toString('utf8'));
+            });
+            ant.on('close', function (code) {
+		console.log(code);
+            console.log("Built jar");
+            
+            // finally upload results to AWS.
+ 	    //./send-to-s3.py path/to/my/local/file "mystore/mybackups"
+            // Then re-run the test.
+               test_results = ""
+               fourpeaks    = spawn('java', ['-cp', 'ABAGAIL.jar', 'opt.test.FourPeaksTest']);
+               fourpeaks.stdout.on('data',function(buf) {
+	          test_results = test_results + buf
+               });
+	       fourpeaks.stderr.on('data',function(buf) {
+	          test_results = test_results + buf
+               });
+               fourpeaks.on('close', function (code) {
+               var fs = require('fs');
+                the_time = new Date().getTime()
+                filename = "/home/tyson/machine_learning/Assignment2/onaclov2000/ABAGAIL/fourpeaks_log_" + the_time + ".txt"
+                fs.writeFile(filename, test_results, function(err) {
+                if(err) {
+                    return console.log(err);
+                  }
+		 send_to_s3    = spawn('./send-to-s3.py', [filename, 'ML/abagail/test_results']);
+                 console.log("The file was saved!");
+		 send_to_s3.stderr.on('data', function(buf){
+                
+		 });
+                 send_to_s3.on('close', function (code) {
+                 
+                 console.log("The file was uploaded!");
+		  }); 
+                }); 
+               console.log("Completed Test Run");
+               //console.log(test_results);
+            // re-run ant, then re-run the test.
+            // finally upload results to AWS.
+ 	    //./send-to-s3.py path/to/my/local/file "mystore/mybackups"
+          
+                });
+             });
+            // finally upload results to AWS.
+ 	    //./send-to-s3.py path/to/my/local/file "mystore/mybackups"
           });
-       }
     }
     initialize = true;
 });
